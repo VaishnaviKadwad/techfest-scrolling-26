117675de289882c868b086fe5d7a5ecf239edd8f
Vaishnavi Kadwad <vaishnavikadwad1@gmail.com>
2026-07-09 11:44:18 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   vite: {
-    base: '/techfest-scrolling-26/', // 👈 This tells Vite your exact repo name
+   
   },
   tanstackStart: {
     // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
